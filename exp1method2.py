cda4a1955afbec30846e09ca6df9ebaaf1191cc0
DUPPANAPUDI LOHITHA <23wh1a05g4@bvrithyderabad.edu.in>
2026-01-05 11:41:03 +0530

    Implement statistical calculations in exp1method2.py

---
+++ exp1method2.py
@@ -0,0 +1,19 @@
+# Method 2:
+
+
+import statistics
+
+data = list(map(int, input("Enter numbers separated by space: ").split()))
+
+mean = statistics.mean(data)
+median = statistics.median(data)
+mode = statistics.mode(data)
+variance = statistics.variance(data)
+std_dev = statistics.stdev(data)
+
+print("\nResults:")
+print("Mean =", mean)
+print("Median =", median)
+print("Mode =", mode)
+print("Variance =", variance)
+print("Standard Deviation =", std_dev)
