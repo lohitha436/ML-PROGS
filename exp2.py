872cb8e05e40ee73f2616b3d908ead8771992634
DUPPANAPUDI LOHITHA <23wh1a05g4@bvrithyderabad.edu.in>
2026-02-09 02:36:01 +0530

    Update exp2.py

--- exp2.py
+++ exp2.py
@@ -1,48 +1,16 @@
-# Import required libraries
 import numpy as np
 import math
-import statistics as stats
-from scipy import special
+from scipy import stats
 
-# Define two arrays
-array1 = np.array([10, 20, 30, 40, 50])
-array2 = np.array([2, 4, 5, 8, 10])
+a = np.array([10, 20, 30])
+b = np.array([2, 4, 6])
 
-# Arithmetic Operations using NumPy
-addition = np.add(array1, array2)
-subtraction = np.subtract(array1, array2)
-multiplication = np.multiply(array1, array2)
-division = np.divide(array1, array2)
-modulus = np.mod(array1, array2)
+print("Addition:", np.add(a,b))
+print("Subtraction:", np.subtract(a,b))
+print("Multiplication:", np.multiply(a,b))
+print("Division:", np.divide(a,b))
+print("Exponentiation:", np.power(a,2))
+print("Modulus:", np.mod(a,b))
 
-# Exponentiation
-# Using NumPy
-exponent_numpy = np.power(array1, 2)
-
-# Using Math (applied element-wise)
-exponent_math = [math.pow(x, 2) for x in array1]
-
-# Using SciPy
-exponent_scipy = special.exp2(array2)  # 2^x for each element
-
-# Statistics operations
-mean_array1 = stats.mean(array1)
-median_array1 = stats.median(array1)
-
-# Display results
-print("Array 1:", array1)
-print("Array 2:", array2)
-
-print("\nAddition:", addition)
-print("Subtraction:", subtraction)
-print("Multiplication:", multiplication)
-print("Division:", division)
-print("Modulus:", modulus)
-
-print("\nExponentiation (NumPy - array1^2):", exponent_numpy)
-print("Exponentiation (Math - array1^2):", exponent_math)
-print("Exponentiation (SciPy - 2^array2):", exponent_scipy)
-
-print("\nStatistics on Array 1:")
-print("Mean:", mean_array1)
-print("Median:", median_array1)
+print("Square root using math:", math.sqrt(25))
+print("Mean using scipy:", stats.tmean(b))
