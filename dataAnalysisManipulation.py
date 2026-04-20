161dcd8ca65b103037529fbdcc56770e6ac856dc
DUPPANAPUDI LOHITHA <23wh1a05g4@bvrithyderabad.edu.in>
2026-04-20 22:53:35 +0530

    Update dataAnalysisManipulation.py

--- dataAnalysisManipulation.py
+++ dataAnalysisManipulation.py
@@ -1,38 +1,27 @@
-# Import required libraries
+
 import pandas as pd
 from sklearn.datasets import load_iris
-
-# Load dataset
 iris = load_iris()
-
-# Convert dataset into Pandas DataFrame
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
 
-# Add target column
 df['species'] = iris.target
 
-# Display first 5 rows
 print("First 5 rows of the dataset:")
 print(df.head())
 
-# Dataset information
 print("\nDataset Information:")
 print(df.info())
 
-# Statistical summary
 print("\nStatistical Summary:")
 print(df.describe())
 
-# Check for missing values
 print("\nMissing Values:")
 print(df.isnull().sum())
 
-# Filter data
 print("\nFiltered Data (Sepal Length > 6):")
 filtered_data = df[df['sepal length (cm)'] > 6]
 print(filtered_data)
 
-# Group data by species and calculate mean
 print("\nMean values grouped by species:")
 grouped_data = df.groupby('species').mean()
 print(grouped_data)
