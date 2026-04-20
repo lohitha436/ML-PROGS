7de171250c7c5995ba77c369f69e9837765640dd
DUPPANAPUDI LOHITHA <23wh1a05g4@bvrithyderabad.edu.in>
2026-04-20 22:32:40 +0530

    Update Arithmetic_Array_Operations.py

--- Arithmetic_Array_Operations.py
+++ Arithmetic_Array_Operations.py
@@ -1,16 +1,11 @@
 import numpy as np
-import math
-from scipy import stats
 
 a = np.array([10, 20, 30])
-b = np.array([2, 4, 6])
+b = np.array([1, 2, 3])
 
-print("Addition:", np.add(a,b))
-print("Subtraction:", np.subtract(a,b))
-print("Multiplication:", np.multiply(a,b))
-print("Division:", np.divide(a,b))
-print("Exponentiation:", np.power(a,2))
-print("Modulus:", np.mod(a,b))
-
-print("Square root using math:", math.sqrt(25))
-print("Mean using scipy:", stats.tmean(b))
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Exponent:", a ** b)
+print("Modulus:", a % b)
